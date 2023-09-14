f3b9efa621ed23e1d249e69277a7bd774acdd56c
Suraj Wate <suraj.wate@gmail.com>
2023-09-14 23:33:04 +0100

    Create stats.py
    Added a qualitative_summary function in stats file.

---
+++ stats.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def qualitative_summary(df, cname):
+    """
+    Creates a qualitative summary of the column cname of the dataframe df.
+
+    Parameters
+    ----------
+    df : pandas dataframe
+        The dataframe to be analyzed
+    cname : string
+        The name of the column to be analyzed
+
+    """
+
+    count = df[cname].value_counts()
+    frequency = pd.DataFrame({cname: count.index, 'Frequency':count.values})
+    frequency['Relative Frequency'] = frequency['Frequency']/frequency['Frequency'].sum()
+    print("Numerical Summary of " + cname + "\n")
+    print(frequency.set_index(cname))
+    print("\n\n")
+
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(12, 5))
+
+    sns.barplot(x=cname, y='Frequency', data=frequency, ax=ax[0])
+    ax[0].set_title("Frequency of " + cname)
+
+    ax[1].pie(frequency['Frequency'], labels=frequency[cname], autopct='%1.1f%%')
+    ax[1].set_title("Relative Frequency of " + cname)
+
+    plt.show()
