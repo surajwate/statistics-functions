4d0cb3e1365016ec8d30cef1fae3455cd6cdce18
Suraj Wate <suraj.wate@gmail.com>
2023-09-15 04:39:24 +0100

    added two_var_qualitative function

--- stats.py
+++ stats.py
@@ -34,3 +34,32 @@ def qualitative_summary(df, cname):
     ax[1].set_title("Relative Frequency of " + cname)
 
     plt.show()
+
+
+def two_var_qualitative(df, var1, var2):
+    """
+    Create bar plots equal to the number of unique values of var1, each of which shows the relative frequency of var2 for a unique value of var1.
+    
+    Parameters
+    ----------
+    df : pandas dataframe
+        The dataframe to be analyzed
+    var1 : string
+        The name of the first column to be analyzed
+    var2 : string
+        The name of the second column to be analyzed
+
+    """
+    order = df[var2].unique().tolist()
+    number_of_plots = df[var1].nunique()
+
+    fig, ax = plt.subplots(nrows=1, ncols=number_of_plots, figsize=(12, 5))
+
+    for i in range(number_of_plots):
+        sdf = df[df[var1] == df[var1].unique()[i]]
+        count = sdf[var2].value_counts()
+        sns.barplot(x=count.index, y=count.values, ax=ax[i], order=order)
+        ax[i].set_xticklabels(ax[i].get_xticklabels(),rotation=45)
+        ax[i].title.set_text(f"Relative Frequency of {var2} for {var1} = {df[var1].unique()[i]}")
+
+    plt.show()
